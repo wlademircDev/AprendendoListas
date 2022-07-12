9bfd434753803b5cf2decfe85344dd1884fbf674
wlade <wlade@Desktop-P4KG4>
2022-07-12 16:40:29 -0300

    Adicionar arquivos de projeto.

---
+++ AprendendoListas/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AprendendoListas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> clientes = new List<string>();
+            clientes.Add("Pedro");
+            clientes.Add("João");
+            clientes.Add("Bleno");
+            clientes.Add("Wlademir");
+
+            foreach (string cliente in clientes)
+            {
+                Console.WriteLine(cliente);
+            }
+           List<string> filtrada = clientes.FindAll(cliente => cliente.Length < 5);
+
+            Console.WriteLine("-----------------");
+
+            foreach (string cliente in filtrada)
+            {
+                Console.WriteLine(cliente);
+            }
+           
+
+            Console.ReadLine();
+        }
+    }
+}
